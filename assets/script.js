578f7896b1e3e6a4fa71bd9460bc67f0d92f8f0a
JonelleH <jonelle.harris04@gmail.com>
2021-09-19 23:56:47 -0400

    added javascript

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,105 @@ var timerEl = document.querySelector("#timer");
 var timeLeft = 60;
 var totalPoints = 0;
 var end = false;
+
+var points = function() {
+    totalPoints = totalPoints + 10;
+    console.log("You have " + totalPoints + " points!");
+}
+var timer = function() {
+    var timeInterval = setInterval(() => {
+        if (end === false) {
+            timerEl.textContent = timeLeft;
+            timeLeft --;
+            return timeLeft;
+        } else {
+            timerEl.textContent = "";
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+    
+    console.log("TIMER STARTED")
+    if (end === true){
+        timerEl.textContent = timeLeft;
+    }
+}
+
+var highScores = function() {
+    
+    var highScoreContainer = document.createElement("div");
+    highScoreContainer.className = "hs-container";
+    mainEl.appendChild(highScoreContainer);
+
+    var title = document.createElement("h1");
+    title.className = "high-score-title";
+    title.textContent = "High Score";
+    highScoreContainer.appendChild(title);
+
+    var initals = localStorage.getItem("initals");
+    var score = localStorage.getItem("score");
+    var leaderboardScore = document.createElement("p");
+    leaderboardScore.className = "leaderboard-score";
+    leaderboardScore.textContent = "1." + initals + " - " + score;
+    highScoreContainer.appendChild(leaderboardScore);
+
+    var leaderboard = document.createElement("div");
+    leaderboard.className = "leaderboard";
+    highScoreContainer.appendChild(leaderboard);
+    
+    var goBack = document.createElement("button");
+    goBack.className = "go-back-btn";
+    goBack.textContent = "Go Back";
+    goBack.addEventListener("click", event => {
+        totalPoints = 0;
+        end = false;
+        timeLeft = 60;
+        highScoreContainer.remove();
+        start();
+    })
+    leaderboard.appendChild(goBack);
+    
+    var clearHighScore = document.createElement("button");
+    clearHighScore.className = "clear-highScore";
+    clearHighScore.textContent = "Clear High Score";
+    clearHighScore.addEventListener("click", event => {
+        
+        localStorage.removeItem("initals")
+        localStorage.removeItem("score")
+        leaderboardScore.remove();
+    })
+    
+    leaderboard.appendChild(clearHighScore);
+}
+
+var enterScore = function() {
+
+    var doneContainer = document.createElement("div");
+    doneContainer.className = "done-container";
+    mainEl.appendChild(doneContainer);
+
+    var done = document.createElement("h1");
+    done.textContent = "All Done!";
+    done.className = "done-title";
+    doneContainer.appendChild(done);
+    
+    var score = document.createElement("h4");
+    score.textContent = "Your final score is " + totalPoints;
+    score.className = "score";
+    doneContainer.appendChild(score); 
+
+    var inputContainer = document.createElement("div");
+    inputContainer.className = "input-container";
+    doneContainer.appendChild(inputContainer);
+
+    var label = document.createElement("label");
+    label.textContent = "Enter Initials:";
+    label.className = "label";
+    label.setAttribute("for", "input");
+    inputContainer.appendChild(label);
+    
+    var initalInput = document.createElement("Input");
+    initalInput.className = "score-input";
+    initalInput.setAttribute("id", "input")
+    initalInput.setAttribute("name", "input");
+    initalInput.setAttribute("type", "text");
+    inputContainer.appendChild(initalInput);
